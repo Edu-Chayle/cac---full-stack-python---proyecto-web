b7e6892a728274bd12390abc081a599027efa0a3
Edu-Chayle <edu.chayle@gmail.com>
2023-12-10 22:58:32 -0300

    Se rediseño la versión móvil del header

--- proyecto web/js/headerYFooter.js
+++ proyecto web/js/headerYFooter.js
@@ -1,9 +1,9 @@
 const largeHeaderContent = `
-<div class="logo-y-nav-i">
-    <div class="header-logo-i">
+<div class="header-container">
+    <div class="header-logo">
         <a href="https://21kmpalermo-administracion.netlify.app"><img src="../img/header-logo.png" alt="Logo 21 km Palermo"></a>
     </div>
-    <nav class="nav-i">
+    <nav class="nav">
         <ul>
             <li><a href="../index.html">Inicio</a></li>
             <li><a href="inscripcion.html">Inscripción</a></li>
@@ -11,19 +11,19 @@ const largeHeaderContent = `
         </ul>
     </nav>
 </div>
-<div class="band-i"></div>
+<div class="band"></div>
 `;
 
 const smallHeaderContent = `
-<div class="logo-y-nav-i">
-    <div class="header-logo-i">
+<div class="header-container">
+    <div class="header-logo">
         <a href="https://21kmpalermo-administracion.netlify.app"><img src="../img/header-logo.png" alt="Logo 21 km Palermo"></a>
     </div>
-    <div class="header-arrows-i">
+    <div class="header-arrows">
         <label for="input-menu"><i class="fas fa-angle-double-down"></i></label>
     </div>
-    <input id="input-menu" class="input-menu-i" type="checkbox" name="menu">
-    <nav class="nav-list-i">
+    <input id="input-menu" class="input-menu" type="checkbox" name="menu">
+    <nav class="nav-list">
         <ul>
             <li><a href="../index.html">Inicio</a></li>
             <li><a href="inscripcion.html">Inscripción</a></li>
@@ -31,7 +31,7 @@ const smallHeaderContent = `
         </ul>
     </nav>
 </div>
-<div class="band-i"></div>
+<div class="band"></div>
 `;
 
 const footerContent = `
@@ -44,7 +44,7 @@ const footerContent = `
 <p>Developer by Team One</p>
 `;
 
-function actualizarHeader() {
+function updateHeader() {
     if (window.innerWidth < 375) {
         document.getElementById("header").innerHTML = smallHeaderContent;
     } else {
@@ -52,7 +52,28 @@ function actualizarHeader() {
     }
 }
 
-window.addEventListener("load", actualizarHeader);
-window.addEventListener("resize", actualizarHeader);
+window.addEventListener("load", updateHeader);
+window.addEventListener("resize", updateHeader);
 
 document.getElementById("footer").innerHTML = footerContent;
+
+document.addEventListener("change", function(event) {
+    if (event.target.matches("#input-menu")) {
+        const arrows = document.querySelector(".fas");
+        const band = document.querySelector(".band");
+        const main = document.querySelector("main");
+        const footer = document.querySelector("footer");
+
+        if (event.target.checked) {
+            arrows.classList.add("active");
+            band.style.top = "126px";
+            main.style.top = "126px";
+            footer.style.top = "126px";
+        } else {
+            arrows.classList.remove("active");
+            band.style.top = "0";
+            main.style.top = "0";
+            footer.style.top = "0";
+        }
+    }
+});
